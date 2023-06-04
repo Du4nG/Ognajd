5ff05efa4453c55ab375445a822582fe3ab0b1ea
Du4nG <tandung5634@gmail.com>
2023-06-04 21:49:24 +0700

    add register, bootstrap 5 crispy

--- zango/zango/urls.py
+++ zango/zango/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # include urls.py trong folder main
     path('', include('main.urls')),
+    
+    path('register/', v.register, name='register'),
+    
 ]
