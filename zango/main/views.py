0a0dab0c736ae1e2a9ece965908320a6bdb74d94
Du4nG <tandung5634@gmail.com>
2023-06-04 19:05:04 +0700

    add bootstrap

--- zango/main/views.py
+++ zango/main/views.py
@@ -10,7 +10,7 @@ def index(response, id):
         print(response.POST)
         if response.POST.get('save'):
             for item in ls.item_set.all():
-                if response.POST.get('c'+str(item.id)) == 'clicked':
+                if response.POST.get('c' + str(item.id)) == 'clicked':
                     item.complete = True
                 else: 
                     item.complete = False
