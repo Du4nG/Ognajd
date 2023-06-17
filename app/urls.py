3575a74c818b2b06531d45895a4ff2abe4ad651f
Du4nG <tandung5634@gmail.com>
2023-06-17 22:36:51 +0700

    adjust product quantity, add crsf

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.udpateItem, name='update_item'),
 ]
